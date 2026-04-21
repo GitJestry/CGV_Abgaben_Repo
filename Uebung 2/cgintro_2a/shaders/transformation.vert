0779e6914affc577c2b4bd9685a781062c9c6954
GitJestry <161480523+GitJestry@users.noreply.github.com>
2026-04-22 00:50:11 +0200

    cgintro_2a erledigt

--- Uebung 2/cgintro_2a/shaders/transformation.vert
+++ Uebung 2/cgintro_2a/shaders/transformation.vert
@@ -4,26 +4,43 @@ layout (location = 0) in vec3 position;
 uniform float uTime;
 
 // Translation
-mat4 mov = mat4( 1. , 0. , 0. , 0.,
-                 0. , 1. , 0. , 0.,
-                 0. , 0. , 1. , 0.,
-                 0. , 0. , 0. , 1.);
-
+mat4 mov;
 // Scaling
-mat4 sca = mat4( 1. , 0. , 0. , 0.,
-                 0. , 1. , 0. , 0.,
-                 0. , 0. , 1. , 0.,
-                 0. , 0. , 0. , 1.);
-
+mat4 sca;
 // Rotation
-mat4 rot = mat4( 1. , 0. , 0. , 0.,
-                 0. , 1. , 0. , 0.,
-                 0. , 0. , 1. , 0.,
-                 0. , 0. , 0. , 1.);
-
+mat4 rot;
 
 void main() {
-    // OpenGL stores matrices in column-major format
-    // So we transpose the each matrix before multiplying to make the notation more intuitive
-    gl_Position = transpose(mov) * transpose(sca) * transpose(rot) * vec4(position.x, position.y, position.z, 1.0);
+    // kreisförmige translation
+    float kreisRadius = 0.6;
+    float tx = kreisRadius * cos(uTime);
+    float ty = kreisRadius * sin(uTime);
+
+    // zoomintervall: [0,5, 0,75]
+    float zoomSpeed = 3.0;
+    float scaling = 0.5 + 0.25 * sin(zoomSpeed * uTime);
+
+    float roationtSpeed = 3;
+    float angle = uTime * roationtSpeed;
+
+    // Translation = Verschiebung
+    // nutzen die homogene form endlich aus
+    mov = mat4( 1.0, 0.0, 0.0, tx,
+                0.0, 1.0, 0.0, ty,
+                0.0, 0.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 1.0 );
+
+    // Skaliere x und y
+    sca = mat4( scaling,   0.0, 0.0, 0.0,
+                0.0, scaling,   0.0, 0.0,
+                0.0, 0.0, 1.0, 0.0,
+                0.0, 0.0, 0.0, 1.0 );
+
+    // rotation wie in der Vorlesung definiert
+    rot = mat4( cos(angle), -sin(angle), 0.0, 0.0,
+                sin(angle),  cos(angle), 0.0, 0.0,
+                0.0,         0.0,        1.0, 0.0,
+                0.0,         0.0,        0.0, 1.0 );
+
+    gl_Position = transpose(mov) * transpose(sca) * transpose(rot) * vec4(position, 1.0);
 }
--- Uebung 2/cgintro_2a/src/main.cpp
+++ Uebung 2/cgintro_2a/src/main.cpp
@@ -1,20 +1,51 @@
 #include <vector>
 
+
 #include <glad/gl.h>
 
 #include <framework/app.hpp>
 #include <framework/mesh.hpp>
 #include <framework/gl/program.hpp>
 
-constexpr float OUTER = 0.5f;
-constexpr float INNER = OUTER / 3.0f;
+constexpr float OUTER = 0.5f; // length
+constexpr float INNER = OUTER / 3.0f; // thickness
 
-const std::vector<float> VERTICES = {
-    // TODO
+// ------- Index Vertex lAYOUT --------
+
+//      0     1           row 0
+// 2    3     4     5     row 1
+// 6    7     8     9     row 2
+//      10    11          row 3
+//
+
+std::vector<float> VERTICES = {
+    // row 0
+    -INNER / 2.0f, OUTER, 0.0f,
+    INNER / 2.0f,  OUTER, 0.0f,
+
+    // row 1
+    -OUTER,        INNER / 2.0f, 0.0f,
+    -INNER / 2.0f, INNER / 2.0f, 0.0f,
+    INNER / 2.0f,  INNER / 2.0f, 0.0f,
+    OUTER,         INNER / 2.0f, 0.0f,
+
+    // row 2
+    -OUTER,        -INNER / 2.0f, 0.0f,
+    -INNER / 2.0f, -INNER / 2.0f, 0.0f,
+    INNER / 2.0f,  -INNER / 2.0f, 0.0f,
+    OUTER,         -INNER / 2.0f, 0.0f,
+
+    // row 3
+    -INNER / 2.0f, -OUTER, 0.0f,
+    INNER / 2.0f,  -OUTER, 0.0f
 };
 
 const std::vector<unsigned int> INDICES = {
-    // TODO
+    0, 3, 1,   1, 3, 4,   // top
+    2, 6, 3,   3, 6, 7,   // left
+    3, 7, 4,   4, 7, 8,   // center
+    4, 8, 5,   5, 8, 9,   // right
+    7, 10, 8,  8, 10, 11  // bottom
 };
 
 struct MainApp : public App {
@@ -22,45 +53,29 @@ struct MainApp : public App {
     Mesh mesh;
     GLint lTime;
 
-    /** 
-     * In the constructor we load all assets, the members' default constructors
-     * implicitly take care of the allocation of OpenGL resources.
-     */
     MainApp() : App(600, 600) {
         // Load, link and compile shader program
         shaderProgram.load("shaders/transformation.vert", "shaders/white.frag");
         // Get uniform location
         lTime = glGetUniformLocation(shaderProgram.handle, "uTime");
-
         mesh.load(VERTICES, INDICES);
-
         // Set background color
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-
-        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Uncomment for wireframe mode
     }
 
     /* Render loop */
     void render() override {
         // Fill screen with the background color (clearing the window)
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // Set the time uniform
-        glProgramUniform1f(shaderProgram.handle, lTime, glfwGetTime());
-
         // Use our shader program
         glUseProgram(shaderProgram.handle);
+        glUniform1f(lTime, glfwGetTime());
+        
         mesh.draw();
     }
 };
 
-#include <filesystem>
-
 int main() {
-#ifndef NDEBUG
-    // cd to parent directory when in debug mode to find resources
-    std::filesystem::current_path(std::filesystem::path(__FILE__).parent_path().parent_path());
-#endif
     MainApp app;
     app.run();
 }
